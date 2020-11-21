07132d3c700739c60cbe2f9af6aeead1fe374eb4
Alex Drozd <drozdster@gmail.com>
2020-11-21 16:38:30 +0100

    hardcoded grocery list

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,50 @@
 <template>
-  <q-page padding>
-    Index
+  <q-page class="q-pa-md">
+    <q-list bordered separator>
+      <q-item
+        v-for="grocery in groceries"
+        :key="grocery.id"
+        clickable
+        @click="grocery.completed = !grocery.completed"
+        :class="grocery.completed ? 'bg-green-1' : 'bg-orange-1'"
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="grocery.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label :class="{
+            'text-strikethrough': grocery.completed
+          }">{{ grocery.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      groceries: [
+        {
+          id: 1,
+          name: 'Apples',
+          completed: false
+        },
+        {
+          id: 2,
+          name: 'Oranges',
+          completed: false
+        },
+        {
+          id: 3,
+          name: 'Butter',
+          completed: false
+        }
+      ]
+    }
+  }
 }
 </script>
 
