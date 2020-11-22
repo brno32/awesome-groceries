6e0e362bc0adddc025b0934ba5a5e1a3badeafa0
Alex Drozd <drozdster@gmail.com>
2020-11-22 23:02:38 +0100

    makign it scrollable

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,46 +1,50 @@
 <template>
-  <q-page class="q-pa-md">
-    <div class="row q-mb-lg">
-      <search></search>
+  <q-page>
+    <div class="q-pa-md absolute full-width full-height column">
+      <div class="row q-mb-lg">
+        <search></search>
+      </div>
+
+      <p
+        v-if="
+          !!search &&
+          Object.keys(groceriesToBuy).length < 1 &&
+          Object.keys(groceriesBought).length < 1
+        "
+      >
+        No search results
+      </p>
+
+      <q-scroll-area class="q-scroll-area-groceries">
+        <no-groceries
+          v-if="Object.keys(groceriesToBuy).length < 1 && !search"
+        ></no-groceries>
+        <groceries-to-buy
+          :groceriesToBuy="groceriesToBuy"
+          v-if="Object.keys(groceriesToBuy).length > 0"
+        ></groceries-to-buy>
+        <groceries-bought
+          class="q-mb-xl"
+          :groceriesBought="groceriesBought"
+          v-if="Object.keys(groceriesBought).length > 0"
+        ></groceries-bought>
+      </q-scroll-area>
+
+      <div class="absolute-bottom text-center q-mb-lg no-pointer-events">
+        <q-btn
+          class="all-pointer-events"
+          round
+          color="primary"
+          size="24px"
+          icon="add"
+          @click="showAddGrocery = true"
+        />
+      </div>
+
+      <q-dialog v-model="showAddGrocery">
+        <add-task @close="showAddGrocery = false"></add-task>
+      </q-dialog>
     </div>
-
-    <p
-      v-if="
-        !!search &&
-        Object.keys(groceriesToBuy).length < 1 &&
-        Object.keys(groceriesBought).length < 1
-      "
-    >
-      No search results
-    </p>
-
-    <div class="relative-position">
-      <no-groceries
-        v-if="Object.keys(groceriesToBuy).length < 1 && !search"
-      ></no-groceries>
-      <groceries-to-buy
-        :groceriesToBuy="groceriesToBuy"
-        v-if="Object.keys(groceriesToBuy).length > 0"
-      ></groceries-to-buy>
-      <groceries-bought
-        :groceriesBought="groceriesBought"
-        v-if="Object.keys(groceriesBought).length > 0"
-      ></groceries-bought>
-    </div>
-
-    <div class="absolute-bottom text-center q-mb-lg">
-      <q-btn
-        round
-        color="primary"
-        size="24px"
-        icon="add"
-        @click="showAddGrocery = true"
-      />
-    </div>
-
-    <q-dialog v-model="showAddGrocery">
-      <add-task @close="showAddGrocery = false"></add-task>
-    </q-dialog>
   </q-page>
 </template>
 
@@ -72,5 +76,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.q-scroll-area-groceries {
+  display: flex;
+  flex-grow: 1;
+}
 </style>
